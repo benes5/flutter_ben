2fec576911f352da0b96a3245ed3d40f35b309ea
Takamasa Miyoshi <phm35via@s.okayama-u.ac.jp>
2022-11-13 06:22:27 +0900

    homeページの追加

---
+++ lib/home.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'bottomtab.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,//右上のDEBUGの文字を消す
+      title: '机に向かわせるアプリ',
+      theme: ThemeData(
+        fontFamily: 'NotoSans',
+        primarySwatch: Colors.indigo,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.indigo,
+          foregroundColor: Colors.white,
+        ),
+      ),
+      home: const BottomTabPage(),
+      
+
+    );
+  }
+}

fee7cd76c260fb35ab227a06d2cef9a89f079794
Takamasa Miyoshi <phm35via@s.okayama-u.ac.jp>
2022-11-13 06:02:27 +0900

    フォントの変更と基準となる色の変更

--- lib/main.dart
+++ lib/main.dart
@@ -22,13 +22,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,//右上のDEBUGの文字を消す
-      title: '観光地アプリ',
+      title: '机に向かわせるアプリ',
       theme: ThemeData(
         fontFamily: 'NotoSans',
         primarySwatch: Colors.indigo,
         visualDensity: VisualDensity.adaptivePlatformDensity,
         appBarTheme: const AppBarTheme(
-          backgroundColor: Color.fromARGB(255, 49, 56, 147),//岡大カラー
+          backgroundColor: Colors.indigo,
           foregroundColor: Colors.white,
         ),
       ),
